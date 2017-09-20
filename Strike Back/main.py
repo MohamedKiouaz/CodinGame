490774d6e389a20ed87b37bc090647e9c8303e30
MohamedKiouaz <mohamed.kiouaz@gmail.com>
2017-09-20 16:57:44 +0200

    Update main.py

--- Strike Back/main.py
+++ Strike Back/main.py
@@ -2,23 +2,40 @@ import sys
 import math
 import numpy as np
 
-# Auto-generated code below aims at helping you parse
-# the standard input according to the problem statement.
+laps = int(input())
+checkpointCount = int(input())
 
-boost = False
+checkpointX, checkpointY = [], []
+for i in range(checkpointCount):
+    x, y = [int(i) for i in input().split()]
+    checkpointX += [x]
+    checkpointY += [y]
+
+boost1 = boost2 = False
+
+def lemme1(x1, y1, x2, y2, R):
+    h = math.hypot(x2 - x1, y2 - y1)
+    return int(x1 + (x2 - x1) / h * R), int(y1 + (y2 - y1) / h * R)
 
 while True:
-    x, y, next_checkpoint_x, next_checkpoint_y, next_checkpoint_dist, next_checkpoint_angle = [int(i) for i in input().split()]
-    opponent_x, opponent_y = [int(i) for i in input().split()]
+    x1, y1, vx1, vy1, angle1, next_checkpoint_1 = [int(i) for i in input().split()]
+    x2, y2, vx2, vy2, angle2, next_checkpoint_2 = [int(i) for i in input().split()]
+    ox1, oy1, ovx1, ovy1, oangle1, onext_checkpoint_1 = [int(i) for i in input().split()]
+    ox2, oy2, ovx2, ovy2, oangle2, onext_checkpoint_2 = [int(i) for i in input().split()]
 
-    print(next_checkpoint_dist, file=sys.stderr)
-    speed = int(40 + 60 * (abs(next_checkpoint_angle) < 90))
-    if next_checkpoint_dist < 1200:
-        speed = 60
-    if boost == False and next_checkpoint_dist > 60000 and abs(next_checkpoint_angle) < 3:
+    targetx1, targety1 = lemme1(checkpointX[next_checkpoint_1], checkpointY[next_checkpoint_1], (checkpointX[(next_checkpoint_1 + 1) % checkpointCount] + x1) / 2, (checkpointY[(next_checkpoint_1 + 1) % checkpointCount] + y1)/2, 400)
+    D1 = math.hypot(x1 - targetx1, y1 - targety1)
+    speed1 = 40 + 60 * (D1 > 1)
+    if(boost1 == False and D1 > 5000):
         speed = "BOOST"
-        boost = True
-    h = math.hypot(next_checkpoint_x - x, next_checkpoint_y - y)
-    next_checkpoint_x -= int((next_checkpoint_x - x) * (h - 600) / h)
-    next_checkpoint_y -= int((next_checkpoint_y - y) * (h - 600) / h)
-    print(next_checkpoint_x, next_checkpoint_y, speed)
+        boost1 = True
+    
+    targetx2, targety2 = lemme1(checkpointX[next_checkpoint_2], checkpointY[next_checkpoint_2], (checkpointX[(next_checkpoint_2 + 1) % checkpointCount] + x2) / 2, (checkpointY[(next_checkpoint_2 + 1) % checkpointCount] + y2)/2, 400)
+    D2 = math.hypot(x2 - targetx2, y2 - targety2)
+    speed2 = 40 + 60 * (D2 > 1)
+    if(boost2 == False and D2 > 5000):
+        speed = "BOOST"
+        boost2 = True
+    print(boost1, boost2, file=sys.stderr)
+    print(targetx1, targety1, speed1)
+    print(targetx2, targety2, speed2)
